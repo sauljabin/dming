2fae1b4aecae7ee34f3dc67fd55bca6e8727a2a5
Saúl Piña <sauljabin@gmail.com>
2025-07-29 20:27:51 -0500

    refactor code

--- dming/dice.py
+++ dming/dice.py
@@ -12,26 +12,16 @@ import random
 
 
 def roll(dice: str) -> tuple[str, int]:
-    def kh(total_keep: int, dice: list[int]) -> list[int]:
-        return heapq.nlargest(total_keep, dice)
-
-    def kl(total_keep: int, dice: list[int]) -> list[int]:
-        return heapq.nsmallest(total_keep, dice)
-
-    def parser(
-        total: str, die: str, keep: str, total_keep: str
-    ) -> tuple[int, int, str, int]:
-        return (
-            int(total) if total else 1,
-            int(die),
-            keep,
-            int(total_keep) if total_keep else 1,
-        )
-
     def replacer(expression: re.Match[str]):
-        total, die, keep, total_keep = parser(*expression.groups())
-        dice = [random.randint(1, die) for _ in range(total)]
-        filtered_dice = {"kh": kh, "kl": kl}[keep](total_keep, dice) if keep else dice
+        total, die, keep, total_keep = expression.groups()
+        dice = [random.randint(1, int(die)) for _ in range(int(total) if total else 1)]
+        filtered_dice = (
+            {"kh": heapq.nlargest, "kl": heapq.nsmallest}[keep](
+                int(total_keep) if total_keep else 1, dice
+            )
+            if keep
+            else dice
+        )
         return str(sum(filtered_dice))
 
     expression = re.sub(r"(\d+)?d(\d+)(kh|kl)?(\d+)?", replacer, dice)
